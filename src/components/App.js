4183ba822247e9bf7cccbe04df2a0d60c32aca7d
Sarvar <r0ssmike727@yandex.com>
2021-03-07 17:01:28 +0500

    Added Sidebar

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Twitter Clone</h1>
+      <Sidebar />
+      {/* Sidebar */}
+      {/* Feed */}
+    </div>
+  );
+}
+
+export default App;
